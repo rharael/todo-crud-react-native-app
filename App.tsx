700887f50bee8d12d59faf51ef0e6c4507d0cdf9
Wendel Rharael <60363332+rharael@users.noreply.github.com>
2024-12-30 08:34:53 -0300

    feat: added icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Icons from './assets/icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Home page!</Text>
+      <Icons.LogoRocket />
+      <Icons.LogoTodo />
+      <Icons.LogoBrand />
+      <Icons.ClipboardTextRegular />
+      <Icons.CircleRegular />
+      <Icons.CheckCircleFill />
+      <Icons.TrashRegular />
+      <Icons.PlusCircleRegular />
+      <Icons.Search />
+      <Icons.Spinner />
+      <Icons.SpinnerGap />
+      <Icons.Exit />
+      <Icons.Close />
+      <Icons.Eye />
+      <Icons.EyeClosed />
       <StatusBar style="auto" />
     </View>
   );
